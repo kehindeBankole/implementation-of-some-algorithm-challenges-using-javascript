295f679db13dfa51b47e36f4e1dded718e3e030a
kehindeBankole <bankolek1@gmail.com>
2022-10-27 11:12:35 +0100

    non repeat susbstring

--- sliding-window/index.js
+++ sliding-window/index.js
@@ -57,6 +57,7 @@ function longest_substring_with_k_distinct(str, k) {
       }
       windowStart += 1; // shrink the window
     }
+    console.log(charFrequency , windowStart , windowEnd)
     // remember the maximum length so far
     maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
   }
@@ -65,9 +66,41 @@ function longest_substring_with_k_distinct(str, k) {
 }
 
 
-console.log(`Length of the longest substring: `
-  + longest_substring_with_k_distinct('araaci', 2));
+// console.log(`Length of the longest substring: `
+//   + longest_substring_with_k_distinct("cbbebi", 3));
 // console.log(`Length of the longest substring: `
 //   + longest_substring_with_k_distinct('araaci', 1));
 // console.log(`Length of the longest substring: `
 //   + longest_substring_with_k_distinct('cbbebi', 3));
+
+
+
+function non_repeat_substring(str) {
+  let windowStart = 0,
+      maxLength = 0,
+      charIndexMap = {};
+
+  // try to extend the range [windowStart, windowEnd]
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+      const rightChar = str[windowEnd];
+      // if the map already contains the 'rightChar', shrink the window from the beginning 
+      // so that we have only one occurrence of 'rightChar'
+      if (rightChar in charIndexMap) {
+        console.log(Math.max(windowStart, charIndexMap[rightChar] + 1))
+          // this is tricky; in the current window, we will not have any 'rightChar' after 
+          // its previous index and if 'windowStart' is already ahead of the last index of 
+          // 'rightChar', we'll keep 'windowStart'
+          windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+      }
+      // insert the 'rightChar' into the map
+      charIndexMap[rightChar] = windowEnd;
+      // remember the maximum length so far
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+  return maxLength;
+}
+
+
+console.log(`Length of the longest substring: ${non_repeat_substring('aabccbb')}`);
+// console.log(`Length of the longest substring: ${non_repeat_substring('abbbb')}`);
+// console.log(`Length of the longest substring: ${non_repeat_substring('abccde')}`);
