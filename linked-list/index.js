ce2671c37ea5d0f6b4c9812ae4ec2877a24aaa78
kehindeBankole <bankolek1@gmail.com>
2022-11-13 19:11:51 +0100

    LL

--- linked-list/index.js
+++ linked-list/index.js
@@ -22,17 +22,43 @@ class SinglyList {
       this.tail = newNode;
     }
     this.length++;
-  //  return this;
+    return this;
+  }
+
+  traverse() {
+    var current = this.head;
+    while (current) {
+      console.log(current);
+      current = current.next;
+    }
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+    console.log(current.val);
+    console.log(newTail.val);
   }
 }
 
 var list = new SinglyList();
+
 list.push("hello");
 list.push("hy");
 list.push("hey");
 list.push("heooy");
-// var first = new Node("hi")
-// first.next=new Node("there")
-// first.next.next=new Node("how")
-
-console.log(list.head.next.next.next);
+list.pop()
+console.log(list);
+// // var first = new Node("hi")
+// // first.next=new Node("there")
+// // first.next.next=new Node("how")
+// list.pop();
+// console.log(list.tail);
