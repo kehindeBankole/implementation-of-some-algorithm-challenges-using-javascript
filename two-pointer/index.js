7b8927a9d33ffdd70a506c75e92fa1a5f288382c
kehindeBankole <bankolek1@gmail.com>
2023-03-05 17:21:43 +0100

    square and sort array

--- two-pointer/index.js
+++ two-pointer/index.js
@@ -3,7 +3,6 @@ function pairWithTargetSum(arr, targetSum) {
   let right = arr.length - 1;
   for (let left = 0; left < right; left++) {
     const currentSum = arr[left] + arr[right];
-    console.log(currentSum);
     if (currentSum === targetSum) {
       return [left, right];
     }
@@ -18,7 +17,6 @@ function pairWithTargetSum(arr, targetSum) {
 pairWithTargetSum([1, 2, 3, 4, 6], 6);
 
 function removeDuplicates(arr) {
-
   //can also be implemented with set , but using this method just to practice 2 pointer
   /*Given an array of sorted numbers, remove all duplicate number instances from it in-place, such that each element appears only once. The relative order of the elements should be kept the same and you should not use any extra space so that that the solution have a space complexity of O(1).
 Move all the unique elements at the beginning of the array and after moving return the length of the subarray that has no duplicate in it*/
@@ -32,3 +30,9 @@ Move all the unique elements at the beginning of the array and after moving retu
   return nextNonDuplicate;
 }
 removeDuplicates([2, 3, 3, 3, 6, 9, 9]);
+
+function squareSortedArray(arr) {
+  //Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+  return arr.map((item) => item * item).sort((a, b) => a - b);
+}
+squareSortedArray([-2, -1, 0, 2, 3]);
