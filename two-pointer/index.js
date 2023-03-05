f5c851af3791580a7d95b851589c30ea33c36f7c
kehindeBankole <bankolek1@gmail.com>
2023-03-05 17:15:53 +0100

    remove duplicate

--- two-pointer/index.js
+++ two-pointer/index.js
@@ -1,7 +1,6 @@
 function pairWithTargetSum(arr, targetSum) {
   //Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target
-  let left = 0,
-    right = arr.length - 1;
+  let right = arr.length - 1;
   for (let left = 0; left < right; left++) {
     const currentSum = arr[left] + arr[right];
     console.log(currentSum);
@@ -16,5 +15,20 @@ function pairWithTargetSum(arr, targetSum) {
   }
   return [-1, -1];
 }
+pairWithTargetSum([1, 2, 3, 4, 6], 6);
 
-console.log(pairWithTargetSum([1, 2, 3, 4, 6], 6));
+function removeDuplicates(arr) {
+
+  //can also be implemented with set , but using this method just to practice 2 pointer
+  /*Given an array of sorted numbers, remove all duplicate number instances from it in-place, such that each element appears only once. The relative order of the elements should be kept the same and you should not use any extra space so that that the solution have a space complexity of O(1).
+Move all the unique elements at the beginning of the array and after moving return the length of the subarray that has no duplicate in it*/
+  let nextNonDuplicate = 1;
+  for (let x = 0; x < arr.length; x++) {
+    if (arr[nextNonDuplicate - 1] !== arr[x]) {
+      arr[nextNonDuplicate] = arr[x];
+      nextNonDuplicate += 1;
+    }
+  }
+  return nextNonDuplicate;
+}
+removeDuplicates([2, 3, 3, 3, 6, 9, 9]);
